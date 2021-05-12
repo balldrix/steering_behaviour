ddb58c9981820d673ea3c0a59a56988ccb3fd00c
balldrix <cballdev@outlook.com>
2021-05-12 08:38:33 +0100

    adds steer and arrive behaviour

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _maxSpeed;
+    [SerializeField] private float _mass;
+    [SerializeField] private float _deceleration;
+    [SerializeField] private float _seekingRadius;
+    [SerializeField] private float _slowingDistance;
+
+    private SteeringBehaviours _steeringBehaviours;
+    private Vector3 _velocity;
+    private Transform _transform;
+
+    public Transform Transform => _transform;
+    public float MaxSpeed => _maxSpeed;
+    public Vector3 Velocity => _velocity;
+    public float Deceleration => _deceleration;
+    public float ApproachRadius => _slowingDistance;
+
+    private void Awake() => _transform = transform;
+
+    private void Start() => _steeringBehaviours = new SteeringBehaviours(this, _target, _seekingRadius);
+
+    private void Update()
+    {
+        Vector3 acceleration = _steeringBehaviours.Calculate() / _mass;
+        _velocity += acceleration * Time.deltaTime;
+        _velocity = Vector3.ClampMagnitude(_velocity, _maxSpeed);
+        _transform.position += _velocity * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/SteeringBehaviours.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class SteeringBehaviours
+{
+    private Agent _agent;
+    private Transform _target;
+    private float _radius;
+
+    public Vector3 Calculate()
+    {
+        var targetPosition = _target.position;
+
+        if(_radius > 0f)
+            targetPosition = OffSetPosition();
+
+        var distance = Vector3.Distance(_target.position, _agent.Transform.position);
+
+        if(distance < _agent.ApproachRadius)
+            return Arrive(targetPosition);
+        else
+            return Seek(targetPosition);
+    }
+
+    private Vector3 OffSetPosition()
+    {
+        var directionToTarget = (_target.position - _agent.Transform.position).normalized;
+        return _target.position - directionToTarget * _radius;
+    }
+
+    public SteeringBehaviours(Agent agent, Transform target, float radius)
+    {
+        _agent = agent;
+        _target = target;
+        _radius = radius;
+    }
+
+    private Vector3 Seek(Vector3 target)
+    {
+        var newDirection = (target - _agent.Transform.position).normalized;
+        var desiredVelocity = newDirection * _agent.MaxSpeed;
+
+        return desiredVelocity - _agent.Velocity;
+    }
+
+    private Vector3 Arrive(Vector3 target)
+    {
+        Vector3 agentPosition = _agent.Transform.position;
+        var distance = Vector3.Distance(target, agentPosition);
+        var desiredVelocity = (target - agentPosition).normalized * _agent.MaxSpeed * (distance / _agent.ApproachRadius);        
+
+        return desiredVelocity - _agent.Velocity;
+    }
+}
